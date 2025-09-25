aa353ff707672b8325fdd32d700d20f584047724
sagar3181 <sagarbadgujar3181@gmail.com>
2025-09-25 01:40:32 -0500

    successful login

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Route } from '@angular/router';
+import { Routes } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  { path: '**', redirectTo: 'auth/login' }, // fallback route
+];
